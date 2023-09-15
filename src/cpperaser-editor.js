e4c9c026d5d1a0619b6fa9b9877435a896898fd5
David Vodnik <davidvodnik@gmail.com>
2023-09-15 09:08:08 +0300

    Add wasm integration logic

--- src/cpperaser-editor.js
+++ src/cpperaser-editor.js
@@ -1,21 +1,62 @@
-import { LitElement, css, html } from 'lit'
-import './prism.js'
-import 'lit-code'
+import { LitElement, css, html } from "lit";
+import Module from "../cpperaser/build/src/cpperaser.js";
+import "./prism.js";
+import "lit-code";
 
 export class DuckCodeEditor extends LitElement {
+  static get properties() {
+    return {
+      code: { type: String },
+    };
+  }
+
+  constructor() {
+    super();
+    Module().then((m) => {
+      this.cpperaser = m;
+      this.updateText(this.code);
+    });
+  }
+
+  generate_interface(name) {
+    const output = this.cpperaser._malloc(65536);
+    const isValid = this.cpperaser.ccall(
+      "parse_interface",
+      "number",
+      ["string", "number"],
+      [name, output],
+    );
+    var outputString = this.cpperaser.UTF8ToString(output);
+    this.cpperaser._free(output);
+    return outputString;
+  }
+
+  updateText(code) {
+    this.code = code;
+    if (this.cpperaser) {
+      this.code = this.generate_interface(code);
+    }
+  }
 
   render() {
     return html`
-      <div class="row">
-        <lit-code linenumbers code="struct Drawable {\n  void draw();\n};" language="cpp"></lit-code>
-        <lit-code linenumbers language="cpp"></lit-code>
+      <div class="editor-group">
+        <lit-code
+          linenumbers
+          code="struct Drawable {
+  void draw();
+};"
+          @update=${({ detail: code }) => this.updateText(code)}
+          language="cpp"
+        ></lit-code>
+        <lit-code linenumbers language="cpp" code="${this.code}"></lit-code>
       </div>
-    `
+    `;
   }
 
   static get styles() {
     return css`
-      .row {
+      .editor-group {
         display: grid;
         grid-template-columns: 1fr 1fr;
         column-gap: 1em;
@@ -25,11 +66,11 @@ export class DuckCodeEditor extends LitElement {
       lit-code {
         border-radius: 8px;
         border: 2px solid #eee;
-        height: 500px;
+        height: 800px;
+        min-width: 100%;
       }
-
-    `
+    `;
   }
 }
 
-window.customElements.define('cpperaser-editor', DuckCodeEditor)
+window.customElements.define("cpperaser-editor", DuckCodeEditor);
