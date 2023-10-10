6a8f32b50d435ef5b763dc745ad596b29131bc43
David Vodnik <davidvodnik@gmail.com>
2023-10-10 16:41:00 +0200

    Replace lit-code editor with a simpler implementation

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import './prism.js'
+
+window.update = function update(text) {
+  let result_element = document.querySelector('#highlighting-content')
+  // Handle final newlines (see article)
+  if (text[text.length - 1] == '\n') {
+    // If the last character is a newline character
+    text += ' ' // Add a placeholder space character to the final line
+  }
+  // Update code
+  result_element.innerHTML = text
+    .replace(new RegExp('&', 'g'), '&')
+    .replace(new RegExp('<', 'g'), '<')
+  // Syntax Highlight
+  Prism.highlightElement(result_element)
+}
+
+window.sync_scroll = function sync_scroll(element) {
+  /* Scroll result to scroll coords of event - sync with textarea */
+  let result_element = document.querySelector('#highlighting')
+  // Get and set x and y
+  result_element.scrollTop = element.scrollTop
+  result_element.scrollLeft = element.scrollLeft
+}
+
+window.check_tab = function check_tab(element, event) {
+  let code = element.value
+  if (event.key == 'Tab') {
+    /* Tab key pressed */
+    event.preventDefault() // stop normal
+    let before_tab = code.slice(0, element.selectionStart) // text before tab
+    let after_tab = code.slice(element.selectionEnd, element.value.length) // text after tab
+    let cursor_pos = element.selectionEnd + 1 // where cursor moves after tab - moving forward by 1 char to after tab
+    element.value = before_tab + '\t' + after_tab // add tab char
+    // move cursor
+    element.selectionStart = cursor_pos
+    element.selectionEnd = cursor_pos
+    update(element.value) // Update text to include indent
+  }
+}
+
+var initial = 'struct Drawable {\n\tvoid draw();\n};'
+update(initial)
+document.querySelector('#editing').value = initial
